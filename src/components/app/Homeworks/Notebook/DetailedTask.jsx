b873f4bd8d9fec5df59bbc739ae642fcca0061a5
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2025-06-18 19:50:59 +0200

    refactor(DetailedTask): make the code working with new data structure and global code improvement (session content and file ahdnling is missing)

--- src/components/app/Homeworks/Notebook/DetailedTask.jsx
+++ src/components/app/Homeworks/Notebook/DetailedTask.jsx
@@ -4,67 +4,58 @@ import EncodedHTMLDiv from "../../../generic/CustomDivs/EncodedHTMLDiv"
 import CheckBox from "../../../generic/UserInputs/CheckBox"
 import { AppContext, SettingsContext, UserDataContext } from "../../../../App"
 import { applyZoom, getZoomedBoudingClientRect } from "../../../../utils/zoom";
-import { Link, useLocation, useNavigate } from "react-router-dom"
 
 import PatchNotesIcon from "../../../graphics/PatchNotesIcon"
 import DownloadIcon from "../../../graphics/DownloadIcon"
 import CopyButton from "../../../generic/CopyButton"
 import { clearHTML } from "../../../../utils/html"
 
+import { useCreateNotification } from "../../../generic/PopUps/Notification"
 import "./DetailedTask.css"
 
-const supposedNoSessionContent = [
+const EMPTY_SESSION_CONTENT = [
     "PHAgc3R5bGU9Ii13ZWJraXQtdGFwLWhpZ2hsaWdodC1jb2xvcjogcmdiYSgwLCAwLCAwLCAwKTsiPjxicj48L3A+PHAgc3R5bGU9Ii13ZWJraXQtdGFwLWhpZ2hsaWdodC1jb2xvcjogcmdiYSgwLCAwLCAwLCAwKTsiPjxicj48L3A+",
     "",
 ]
 
-export default function DetailedTask({ task, day, ...props }) {
+export default function DetailedTask({ task, ...props }) {
     const { usedDisplayTheme } = useContext(AppContext);
-    
+
     const userData = useContext(UserDataContext);
     const {
-        homeworks: {value: homeworks, set: setHomeworks}
+        homeworks: { value: homeworks, set: setHomeworks },
+        activeHomeworkId: { set: setActiveHomeworkId }
     } = userData;
 
     const settings = useContext(SettingsContext);
     const { isPartyModeEnabled, displayMode, isStreamerModeEnabled } = settings.user;
-    
-    const navigate = useNavigate();
-    const location = useLocation();
+
     const detailedTaskRef = useRef(null);
-    const contentLoadersRandomValues = useRef({ labelWidth: Math.floor(Math.random() * 150) + 100, contentHeight: Math.floor(Math.random() * 200) + 50 })
+    const contentLoadersRandomValues = useRef({ labelWidth: Math.floor(Math.random() * 150) + 100, contentHeight: Math.floor(Math.random() * 200) + 50 });
 
-    function scrollIntoViewNearestParent(element) {
-        if (!element) {
-            return;
-        }
-        const parent = element.parentElement;
-        const parentBounds = getZoomedBoudingClientRect(parent.getBoundingClientRect());
-        const bounds = getZoomedBoudingClientRect(element.getBoundingClientRect());
-
-        parent.scrollTo(0, bounds.y - parentBounds.y + parent.scrollTop - 20)
-    }
-
-    function checkTask(date, task) {
+    function checkTask(task) {
         const isTaskDone = task.isDone;
         task.check()
-        .catch((error) => {
-            console.error(error);
-            homeworks[date].find((item) => item.id === task.id).isDone = isTaskDone;
-            setHomeworks(homeworks);
-        });
-        homeworks[date].find((item) => item.id === task.id).isDone = !isTaskDone;
+            .catch((error) => {
+                console.error(error);
+                task.isDone = isTaskDone;
+                setHomeworks(homeworks);
+            });
+        task.isDone = !isTaskDone;
         setHomeworks(homeworks);
     }
 
-    return <>{(task?.content
-        ? <div ref={detailedTaskRef} onClick={(e) => {navigate(`#${day};${task.id}`); e.stopPropagation()}} className={`detailed-task ${task.isDone ? "done" : ""}`} id={"task-" + task.id} {...props} >
+    // !:!
+    const summonNotification = useCreateNotification();
+
+    return task?.content
+        ? <div ref={detailedTaskRef} onClick={(e) => { setActiveHomeworkId(task.id); e.stopPropagation() }} className={`detailed-task ${task.isDone ? "done" : ""}`} id={"task-" + task.id} {...props} >
             <div className="task-header">
                 <CheckBox
                     id={"task-cb-" + task.id}
-                    confetti={{ onCheck:  displayMode.value === "quality" && isPartyModeEnabled.value }}
+                    confetti={{ onCheck: displayMode.value === "quality" && isPartyModeEnabled.value }}
                     label="Effectué"
-                    onChange={() => { checkTask(day, task) }}
+                    onChange={() => { checkTask(task) }}
                     checked={task.isDone}
                     onClick={(event) => { event.stopPropagation() }}
                 />
@@ -78,8 +69,10 @@ export default function DetailedTask({ task, day, ...props }) {
             </div>
             <EncodedHTMLDiv className="task-content" nonEncodedChildren={<CopyButton content={clearHTML(task.content, undefined, false).innerText} />} backgroundColor={usedDisplayTheme === "dark" ? "#40405b" : "#e4e4ff"} >{task.content}</EncodedHTMLDiv>
             <div className="task-footer">
-                <Link to={`#${day};${task.id};s`} onClick={(e) => e.stopPropagation()} replace={true} className={`task-footer-button ${supposedNoSessionContent.includes(task.sessionContent) ? "disabled" : ""}`}><PatchNotesIcon className="session-content-icon" />Contenu de séance</Link>
-                <Link to={`#${day};${task.id};f`} onClick={(e) => e.stopPropagation()} replace={true} className={`task-footer-button ${task.files.length === 0 ? "disabled" : ""}`}><DownloadIcon className="download-icon" />Fichiers</Link>
+                {/* <Link to={`#${day};${task.id};s`} onClick={(e) => e.stopPropagation()} replace={true} className={`task-footer-button ${EMPTY_SESSION_CONTENT.includes(task.sessionContent) ? "disabled" : ""}`}><PatchNotesIcon className="session-content-icon" />Contenu de séance</Link>
+                <Link to={`#${day};${task.id};f`} onClick={(e) => e.stopPropagation()} replace={true} className={`task-footer-button ${task.files.length === 0 ? "disabled" : ""}`}><DownloadIcon className="download-icon" />Fichiers</Link> */}
+                <button onClick={(e) => {summonNotification(<p>Il faut mettre les contenus de séance</p>, {timer: 3000}); e.stopPropagation()}} disabled={EMPTY_SESSION_CONTENT.includes(task.sessionContent)} className={`task-footer-button ${EMPTY_SESSION_CONTENT.includes(task.sessionContent) ? "disabled" : ""}`}><PatchNotesIcon className="session-content-icon" />Contenu de séance</button>
+                <button onClick={(e) => {summonNotification(<p>Il faut mettre les fichiers</p>, {timer: 3000}); e.stopPropagation()}} disabled={task.files.length === 0} className={`task-footer-button ${task.files.length === 0 ? "disabled" : ""}`}><DownloadIcon className="download-icon" />Fichiers</button>
             </div>
         </div>
         : <div className={`detailed-task`} {...props} >
@@ -104,7 +97,7 @@ export default function DetailedTask({ task, day, ...props }) {
                     backgroundColor={'#7e7eab7F'}
                     foregroundColor={'#9a9ad17F'}
                     height="14"
-                    style={{ width: contentLoadersRandomValues.current.labelWidth/1.5 + "px" }}
+                    style={{ width: contentLoadersRandomValues.current.labelWidth / 1.5 + "px" }}
                 >
                     <rect x="0" y="0" rx="5" ry="5" style={{ width: "100%", height: "100%" }} />
                 </ContentLoader>
@@ -115,6 +108,4 @@ export default function DetailedTask({ task, day, ...props }) {
                 <div className={`task-footer-button disabled`}><DownloadIcon className="download-icon" />Fichiers</div>
             </div>
         </div>
-    )}
-    </>
 }
