909eb52c0ea6e4badbc0f87ef1a86e9dee4788f3
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2025-12-01 08:08:02 +0100

    feat: improve ApiClient

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -7,7 +7,7 @@ export default class EDApiClient {
 	private doubleAuthToken: string = "";
 	private abortControllers: Array<AbortController> = [];
 
-	private constructor() {}
+	private constructor() { }
 
 	static getInstance(): EDApiClient {
 		if (!EDApiClient.instance) {
@@ -24,31 +24,49 @@ export default class EDApiClient {
 		this.doubleAuthToken = t;
 	}
 
+	abort(reason?: any) {
+		this.abortControllers.forEach((abortController) => abortController.abort(reason));
+	}
+
 	private buildUrl(path: string) {
 		const url = new URL(path, apiBase);
 		url.searchParams.append("v", apiVersion);
 		return url;
 	}
 
+	private buildRequestInit(method: "GET" | "HEAD"): RequestInit
+	private buildRequestInit(method: "POST" | "PUT" | "PATCH" | "DELETE", body: any): RequestInit
+	private buildRequestInit(method: string, body?: any): RequestInit {
+		const init: RequestInit = {
+			method,
+		};
+
+		init.headers = new Headers();
+		init.headers.append("x-token", this.token);
+		init.headers.append("2fa-token", this.doubleAuthToken);
+
+		const abortController = new AbortController();
+		this.abortControllers.push(abortController);
+		init.signal = abortController.signal;
+
+		if (body === undefined) {
+			init.headers.append("content-type", "application/x-www-form-urlencoded");
+			init.body = new URLSearchParams();
+			init.body.append("data", JSON.stringify(body));
+		}
+		return init;
+	}
+
 	async post(path: string, body: any): Promise<any> {
-		const usedUrl = this.buildUrl(path);
-
-		const usedHeaders = new Headers();
-		usedHeaders.append("x-token", this.token);
-		usedHeaders.append("2fa-token", this.doubleAuthToken);
-		usedHeaders.append("content-type", "application/x-www-form-urlencoded");
-
-		const requestBody = new URLSearchParams();
-		requestBody.append("data", JSON.stringify(body));
+		const url = this.buildUrl(path);
+		const init = this.buildRequestInit("POST", body);
 
 		try {
-			const res = await fetch(usedUrl, {
-				method: "POST",
-				headers: usedHeaders,
-				body: requestBody
-			});
+			const res = await fetch(url, init);
 			return res.json();
 		} catch (error) {
+			if (!(error instanceof Error)) throw error;
+			if (error.name === "AbortError") throw error;
 			throw new FetchError("Problem occured while fetching to Ed's API", { cause: error });
 		}
 	}
