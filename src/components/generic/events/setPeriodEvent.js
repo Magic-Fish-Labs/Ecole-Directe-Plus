e30f7351bfa98ad07628615da0df001384734092
crapi <crapioutch@gmail.com>
2026-04-05 11:14:31 +0200

    feat(easter): initialize easter event period and plan

--- src/components/generic/events/setPeriodEvent.js
+++ src/components/generic/events/setPeriodEvent.js
@@ -1,4 +1,9 @@
 function getCurrentPeriodEvent() {
+    const forcedEvent = localStorage.getItem("forcedPeriodEvent");
+    if (forcedEvent) {
+        return forcedEvent;
+    }
+
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
@@ -7,6 +12,11 @@ function getCurrentPeriodEvent() {
             name: 'christmas',
             start: new Date(today.getFullYear(), 11, 1, 0, 0, 0),
             end: new Date(today.getFullYear(), 11, 31, 0, 0, 0)
+        },
+        easter: {
+            name: 'easter',
+            start: new Date(today.getFullYear(), 3, 1, 0, 0, 0),
+            end: new Date(today.getFullYear(), 3, 30, 0, 0, 0)
         }
     };
 
